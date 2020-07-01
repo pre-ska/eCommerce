6dbe953e8300c4b98c5cc781008f202c9af4a935
pre-ska <wolfwar@outlook.com>
2020-07-01 14:23:15 +0200

    persist local storage cart sa redux-persistor

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist"; //9-2
+import storage from "redux-persist/lib/storage"; //9-2
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+
+//9-2
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist"; //9-2
 
 import rootReducer from "./rootReducer";
 
@@ -7,4 +8,6 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store); //9-2
+
+export { store, persistor };
